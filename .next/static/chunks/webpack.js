100c4d123550646192cc264e5e5cdf22d61911f3
luaguero <lucas.aguero98@gmail.com>
2023-02-25 14:31:14 -0300

    toggle navBar css

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "05ef0fb0f1d438d8"; }
+/******/ 		__webpack_require__.h = function() { return "d5dad9f469e7225e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
